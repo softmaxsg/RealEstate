15e545aa0158c7309e84c07f48ced9994cd9bb0b
Vitaly Chupryk <vitaly@softmaxsg.com>
2018-12-20 20:17:10 +0100

    Add `PropertiesPresenter` with required tests

--- RealEstate/UserStories/PropertiesList/PropertiesPresenter.swift
+++ RealEstate/UserStories/PropertiesList/PropertiesPresenter.swift
@@ -12,3 +12,57 @@ protocol PropertiesPresenterProtocol {
     func configure(item: PropertyItemViewProtocol, at index: Int)
 
 }
+
+final class PropertiesPresenter: PropertiesPresenterProtocol {
+    
+    private weak var view: PropertiesViewProtocol?
+    private let gateway: PropertiesGatewayProtocol
+    private let priceFormatter: NumberFormatter
+
+    private var properties: [Property] = []
+
+    init(view: PropertiesViewProtocol, gateway: PropertiesGatewayProtocol, priceFormatter: NumberFormatter) {
+        self.view = view
+        self.gateway = gateway
+        self.priceFormatter = priceFormatter
+    }
+    
+    var propertiesCount: Int { return properties.count }
+    
+    func displayProperties() {
+        gateway.loadAll { [weak self] result in
+            switch result {
+            case .success(let properties):
+                self?.handleLoadedProperties(properties)
+                
+            case .failure(let error):
+                self?.handleLoadingError(error)
+            }
+        }
+    }
+    
+    func configure(item: PropertyItemViewProtocol, at index: Int) {
+        guard properties.indices.contains(index) else { assertionFailure("Index \(index) is out of bounds"); return }
+        let property = properties[index]
+        item.display(item: PropertyItem(
+            title: property.title,
+            address: property.location.address,
+            price: priceFormatter.string(from: NSDecimalNumber(decimal: property.price)) ?? "",
+            image: property.images.first?.url
+        ))
+    }
+    
+}
+
+extension PropertiesPresenter {
+    
+    private func handleLoadedProperties(_ properties: [Property]) {
+        self.properties = properties
+        view?.updateView()
+    }
+    
+    private func handleLoadingError(_ error: Error) {
+        view?.displayLoadingError(error.localizedDescription)
+    }
+    
+}
---
+++ RealEstateTests/UserStories/PropertiesList/PropertiesPresenterTests.swift
@@ -0,0 +1,120 @@
+//
+//  Copyright © 2018 Vitaly Chupryk. All rights reserved.
+//
+
+import XCTest
+@testable import RealEstate
+
+final class PropertiesPresenterTests: XCTestCase {
+    
+    private let properties = Array(repeating: 0, count: Int.random(in: 1...10)).map { _ in Property.random() }
+    private lazy var priceFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "EUR"
+        return formatter
+    }()
+    
+    func testDispslayPropertiesSuccessful() {
+        let presenter = presenterLoadProperties(result: .success(properties))
+        XCTAssertEqual(presenter.propertiesCount, properties.count)
+    }
+    
+    func testDispslayPropertiesFailed() {
+        _ = presenterLoadProperties(result: .failure(MockError.some))
+        // Assert is not needed since validity of the error message is tested in `presenterLoadProperties`
+    }
+    
+    func testConfigureItem() {
+        let presenter = presenterLoadProperties(result: .success(properties))
+        
+        let itemIndex = Int.random(in: 0..<properties.count)
+        let item = configureItem(presenter: presenter, at: itemIndex)
+        let property = properties[itemIndex]
+
+        XCTAssertEqual(item.title, property.title)
+        XCTAssertEqual(item.address, property.location.address)
+        XCTAssertEqual(item.price, priceFormatter.string(from: NSDecimalNumber(decimal: property.price)))
+        XCTAssertEqual(item.image, property.images.first?.url)
+    }
+
+}
+
+extension PropertiesPresenterTests {
+    
+    private enum MockError: Error { case some }
+    
+    private class EmptyTask: Cancellable {
+        func cancel() { }
+    }
+    
+    private func mockedGateway(result: Result<[Property]>, expectation: XCTestExpectation) -> PropertiesGatewayProtocol {
+        return PropertiesGatewayMock { handler in
+            OperationQueue.main.addOperation {
+                handler(result)
+                expectation.fulfill()
+            }
+            
+            return EmptyTask()
+        }
+    }
+    
+    private func mockedViewSuccessState(expectation: XCTestExpectation, file: StaticString = #file, line: UInt = #line) -> PropertiesViewProtocol {
+        return PropertiesViewMock(
+            updateView: { expectation.fulfill() },
+            displayLoadingError: { _ in XCTFail("Should not be called in this test", file: file, line: line) }
+        )
+    }
+    
+    private func presenterLoadProperties(result: Result<[Property]>, file: StaticString = #file, line: UInt = #line) -> PropertiesPresenterProtocol {
+        let viewExpectation = self.expectation(description: "PropertiesView.updateView or displayLoadingError")
+        let gatewayExpectation = self.expectation(description: "PropertiesGateway.loadAll")
+        
+        let viewMock = PropertiesViewMock(
+            updateView: {
+                switch result {
+                case .success:
+                    viewExpectation.fulfill()
+                case .failure:
+                    XCTFail("Should not be called in this test", file: file, line: line)
+                }
+            },
+            displayLoadingError: { errorMessage in
+                switch result {
+                case .success:
+                    XCTFail("Should not be called in this test", file: file, line: line)
+                case .failure(let error):
+                    XCTAssertEqual(errorMessage, error.localizedDescription, file: file, line: line)
+                    viewExpectation.fulfill()
+                }
+            }
+        )
+        
+        let gatewayMock = mockedGateway(result: result, expectation: gatewayExpectation)
+        
+        let presenter = PropertiesPresenter(
+            view: viewMock,
+            gateway: gatewayMock,
+            priceFormatter: priceFormatter
+        )
+        
+        presenter.displayProperties()
+        wait(for: [viewExpectation, gatewayExpectation], timeout: 1)
+        
+        return presenter
+    }
+    
+    private func configureItem(presenter: PropertiesPresenterProtocol, at index: Int) -> PropertyItem {
+        let expectation = self.expectation(description: "PropertyItemView.displayItem")
+        var result: PropertyItem!
+        
+        presenter.configure(item: PropertyItemViewMock {
+            result = $0
+            expectation.fulfill()
+        }, at: index)
+        
+        wait(for: [expectation], timeout: 1)
+        return result
+    }
+
+}
